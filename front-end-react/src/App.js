d73dd4a519a4f1c4be73ffda668f61ebc61fa681
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-25 15:04:24 -0500

    Agregada función para editar y eliminar Todos

--- front-end-react/src/App.js
+++ front-end-react/src/App.js
@@ -12,23 +12,28 @@ const HOST_API = "http://localhost:8080/api";
 // Estados iniciales
 const initialState = {
   list: [],
+  item: {},
 };
 
 // Donde se guardan los estados
 const Store = createContext(initialState);
-
 
 /**
  * Componente para ver todos e ingresar uno nuevo.
  */
 const Form = () => {
   const formRef = useRef(null);
-  const { dispatch } = useContext(Store);
-  const [state, setState] = useState();
+  const {
+    dispatch,
+    state: { item },
+  } = useContext(Store);
+  const [state, setState] = useState(item);
 
+  /**
+   * Método para agregar un nuevo Todo
+   */
   const onAdd = (event) => {
     event.preventDefault();
-
     const request = {
       name: state.name,
       id: null,
@@ -52,16 +57,46 @@ const Form = () => {
       });
   };
 
+  /**
+   * Método para editar un Todo
+   */
+  const onEdit = (event) => {
+    event.preventDefault();
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted,
+    };
+
+    // Hace una petición PUT a la API de forma asíncrona para editar un todo
+    fetch(HOST_API + "/todo", {
+      // Fetch es una promesa, es decir, que solo se ejecutará cuando se resuelva
+      method: "PUT", // Se establece que es una petición PUT
+      body: JSON.stringify(request), // Se establece que la petición necesita un body
+      headers: {
+        "Content-Type": "application/json", // Se establece que la petición enviará un objeto de tipo JSON
+      },
+    })
+      .then((response) => response.json()) // La respuesta se convierte en un objeto JSON
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  };
+
   return (
     <form ref={formRef}>
       <input
         type="text"
         name="name"
+        defaultValue={item.name}
         onChange={(event) => {
           setState({ ...state, name: event.target.value });
         }}
       ></input>
-      <button onClick={onAdd}>Agregar</button>
+      {item.id && <button onClick={onEdit}>Actualizar</button>}
+      {!item.id && <button onClick={onAdd}>Agregar</button>}
     </form>
   );
 };
@@ -74,14 +109,30 @@ const List = () => {
 
   // Hace una petición GET a la API de forma asíncrona para obtener los todo
   useEffect(() => {
-    // Fetch es una promesa, es decir, que solo se ejecutará cuando se resuelva
-    fetch(HOST_API + "/todos")
+    fetch(HOST_API + "/todos") // Fetch es una promesa, es decir, que solo se ejecutará cuando se resuelva
       .then((response) => response.json()) // La respuesta se convierte en un objeto JSON
       .then((list) => {
         // list es la respuesta obtenida de la API
         dispatch({ type: "update-list", list });
       });
   }, [state.list.length, dispatch]); // Condiciones para que funcione el efecto
+
+  /**
+   * Hace una petición DELETE a la API de forma asíncrona para eliminar un todo mediante un id recibido como parámetro
+   * @param {*} id
+   */
+  const onDelete = (id) => {
+    fetch(HOST_API + "/todo" + "/" + id, {
+      // Fetch es una promesa, es decir, que solo se ejecutará cuando se resuelva
+      method: "DELETE", // Se establece que es una petición DELETE
+    }).then((list) => {
+      dispatch({ type: "delete-item", id });
+    });
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo });
+  };
 
   return (
     <div>
@@ -99,7 +150,13 @@ const List = () => {
               <tr key={todo.id}>
                 <td>{todo.id}</td>
                 <td>{todo.name}</td>
-                <td>{todo.isCompleted}</td>
+                <td>{todo.isCompleted === true ? "SÍ" : "NO"}</td>
+                <td>
+                  <button onClick={() => onDelete(todo.id)}>Eliminar</button>
+                </td>
+                <td>
+                  <button onClick={() => onEdit(todo)}>Editar</button>
+                </td>
               </tr>
             );
           })}
@@ -116,8 +173,23 @@ const List = () => {
  */
 function reducer(state, action) {
   switch (action.type) {
+    case "update-item":
+      const listUpdateEdit = state.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {} };
+    case "delete-item":
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      return { ...state, list: listUpdate };
     case "update-list":
       return { ...state, list: action.list };
+    case "edit-item":
+      return { ...state, item: action.item };
     case "add-item":
       const newList = state.list;
       newList.push(action.item);
@@ -128,7 +200,7 @@ function reducer(state, action) {
 }
 
 /**
- * Permite conectar varios componentes.
+ * El provider permite conectar varios componentes.
  */
 const StoreProvider = ({ children }) => {
   // Implementa la lógica de los estados y el dispatch
